15cbbda84a068994d9044c620bd027ccd8fcb9f3
LinkedLst <sarang15mar@gmail.com>
2024-09-08 22:39:32 +0530

    socket.io boilerplate

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+const http=require("http");
+
+const socketio=require("socket.io")
+const server=http.createServer(app);
+const io=socketio(server)
+
+
+
+app.get('/', function (req, res) {
+  res.send('Hi')
+})
+
+
+
+server.listen(3000)
